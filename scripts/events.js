9749bc40af7bc2c17e481e23526e35dcc7d1e229
Hernandez-Fernando <chivista3001@icloud.com>
2017-05-30 06:12:29 -0700

    Events File Cretation

---
+++ scripts/events.js
@@ -0,0 +1,21 @@
+// This is a JS file
+
+var myButton = document.getElementById('myFirstEvent');
+
+function myEventTest() {
+	document.getElementById('test').innerHTML = "If you see this text, that means that your code worked";
+}
+
+myButton.addEventListener("click", myEventTest, false);
+myButton.addEventListener("click", doubleClick, false);
+
+var button2 = document.getElementById('secondButton');
+
+function doubleClick() {
+	document.getElementById('test2').innerHTML = "This is my second Test.";
+};
+
+window.onload = document.getElementById('test').innerHTML = "The page has full loaded";
+window.onresize = function() {
+	document.getElementById('test2').innerHTML = "Now you resized the browser";
+}
