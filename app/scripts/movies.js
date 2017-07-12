3a4b0eebb22944b522612a8f954b3193e81e67b3
Hernandez-Fernando <chivista3001@icloud.com>
2017-07-12 12:42:41 -0700

    Update

--- app/scripts/movies.js
+++ app/scripts/movies.js
@@ -4,6 +4,7 @@
 //var id = 100;
 
 var movies;
+var customers;
 
 function movie(id, name, gener, published, clasification, format, copies){
 	this.id = id;
@@ -27,7 +28,7 @@ movies[2] = {id:102, name:"Fury", gener:"Fiction", years:"2014", clasification:"
 
 
 //var customers = JSON.parse(localStorage.getItem("customers"));
-var customers;
+
 
 //var customersLast = customers.length;
 //var id = 100;
@@ -43,10 +44,10 @@ function Customers(id, name, last, dob, phone, address, city, state, zcode) {
 	this.state = zcode;
 	this.status = "Active";
 }
-
+/*
 customers[0] = {id:100, name:"Fernando", last:"Hernandez", dob:"Sep 26, 1993", phone:"123-456-7890", address:"1234 N Example", city:"Sample", state:"ETC", zcode:"85201", status:"Active"};
 customers[1] = {id:101, name:"Customer", last:"Test", dob:"Jan 12, 1979", phone:"321-456-7890", address:"2134 N Example", city:"Sample", state:"ETC", zcode:"85201", status:"Suspended"};
-customers[2] = {id:102, name:"Test", last:"Customer", dob:"May 4, 1987", phone:"231-456-7890", address:"3214 N Example", city:"Sample", state:"ETC", zcode:"85201", status:"Canceled"};
+customers[2] = {id:102, name:"Test", last:"Customer", dob:"May 4, 1987", phone:"231-456-7890", address:"3214 N Example", city:"Sample", state:"ETC", zcode:"85201", status:"Canceled"}; */
 
 // Customers Functions //
 
