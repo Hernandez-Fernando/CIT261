5e7876e6cd572997e2bf4342ff68b821ca6a8788
Hernandez-Fernando <chivista3001@icloud.com>
2017-07-12 20:59:07 -0700

    Update

--- app/scripts/requests.js
+++ app/scripts/requests.js
@@ -40,7 +40,7 @@ for (var i = 0; i < 8; i++) {
 request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         text = this.responseText;
-		//document.getElementById('app').innerHTML += text;
+		// document.getElementById('app').innerHTML += text;
     } // End of " If " statement
 	
 	switch (i) {
