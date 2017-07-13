1f0e26ccc33fab18d9474a3964c0c9db3eab010b
Hernandez-Fernando <chivista3001@icloud.com>
2017-07-13 13:42:34 -0700

    Update
    Fixed loading pages function. Clean Version. HTML, CSS, and JS files
    for specific files making easier to maintain the app.

--- app/scripts/requests.js
+++ app/scripts/requests.js
@@ -1,86 +1,106 @@
-function showVariables() {
-	
-	/* if (customersHTML != "") {
-	document.getElementById('test').innerHTML = customersHTML;
-	} else {
-		document.getElementById('test').innerHTML = "No Data";
-	} */
-	document.getElementById('test').innerHTML = markupHTML[3];
-}
-//
-//Normal
+var markupHTML = new Array(7);
 
-
-var customersHTML = "";
-var customersNewHTML = ""; 
-var moviesHTML = "";
-var moviesNewHTML = "";
-var settingsHTML = "";
-var homeHTML = "";
-var aboutHTML = "";
-var text = "";
-var j = 0;
-var i = 0;
-var markupHTML = [];
-//Request Customers
+//Request Files
 function loadRequest() {
-/* var request;
-var requestURL;
-for (i; i < 7; i++) {
-	switch (i) {
-		case 0:
-			requestURL = 'pages/home.txt';
-			break;
-		case 1:
-			requestURL = 'pages/customers.txt';
-			break;
-		case 2:
-			requestURL = 'pages/newCustomers.txt';
-			break;
-		case 3:
-			requestURL = 'pages/movies.txt';
-			break;
-		case 4:
-			requestURL = 'pages/addmovie.txt';
-			break;
-		case 5:
-			requestURL = 'pages/settings.txt';
-			break;
-		default:
-			requestURL = 'pages/about.txt';
-			break;
-			} //End of Switch
 	
-	request = new XMLHttpRequest();
-	
-	
-request.onreadystatechange = function() {
+// Home Page Request
+var homerequest = new XMLHttpRequest();
+var homeURL = 'pages/home.txt';
+
+homerequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     text = this.responseText;
-     markupHTML.push(text);
- 	//document.getElementById('test').innerHTML += text;
-	 
+        text = this.responseText;
+        markupHTML[0] = text;
+		document.getElementById('app').innerHTML = text;
+    } 
+};
 
-		
-	} // End of " If " statement
-	document.getElementById('test').innerHTML += markupHTML.length + " - " + requestURL + " - " + text + '<br>';
-	
-}; //End of onready function
+homerequest.open('GET', homeURL, true);
+homerequest.send();
 
+// Customers Page Request
+var customersrequest = new XMLHttpRequest();
+var customersURL = 'pages/customers.txt';
 
-
-} */
-
-var request = new XMLHttpRequest();
-var requestURL = 'pages/home.txt';
-request.onreadystatechange = function() {
+customersrequest.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     text = this.responseText;
-     markupHTML[0] = text;
- 	}
- };
-request.open('GET', requestURL, true);
-request.send();
+        text = this.responseText;
+        markupHTML[1] = text;
+    } 
+};
+
+customersrequest.open('GET', customersURL, true);
+customersrequest.send();
+
+// New Customers Page Request
+var newCrequest = new XMLHttpRequest();
+var newCURL = 'pages/newCustomers.txt';
+
+newCrequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        text = this.responseText;
+        markupHTML[2] = text;
+    } 
+};
+
+newCrequest.open('GET', newCURL, true);
+newCrequest.send();
+
+// Movies Page Request
+var moviesrequest = new XMLHttpRequest();
+var moviesURL = 'pages/movies.txt';
+
+moviesrequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        text = this.responseText;
+        markupHTML[3] = text;
+    } 
+};
+
+moviesrequest.open('GET', moviesURL, true);
+moviesrequest.send();
+
+// Add Movie Page Request
+var addMrequest = new XMLHttpRequest();
+var addMURL = 'pages/addmovie.txt';
+
+addMrequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        text = this.responseText;
+        markupHTML[4] = text;
+    } 
+};
+
+addMrequest.open('GET', addMURL, true);
+addMrequest.send();
+
+// Settings Page Request
+var settingsrequest = new XMLHttpRequest();
+var settingsURL = 'pages/settings.txt';
+
+settingsrequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        text = this.responseText;
+        markupHTML[5] = text;
+    } 
+};
+
+settingsrequest.open('GET', settingsURL, true);
+settingsrequest.send();
+
+// About Page Request
+var aboutrequest = new XMLHttpRequest();
+var aboutURL = 'pages/about.txt';
+
+aboutrequest.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        text = this.responseText;
+        markupHTML[6] = text;
+    } 
+};
+
+aboutrequest.open('GET', aboutURL, true);
+aboutrequest.send();
 
 }
 
@@ -91,8 +111,8 @@ function loadHTML(request) {
 	var requested = "";
 	requested = markupHTML[request];
 	
-document.getElementById('respondMenu').classList.remove('navDown');
-	document.getElementById('app').innerHTML= requested;
+	document.getElementById('respondMenu').classList.remove('navDown');
+	document.getElementById('app').innerHTML = requested;
 }
 
 
