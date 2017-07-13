a4a6a5a8b77b56e87a16ff75ae1123a6c9543247
Hernandez-Fernando <chivista3001@icloud.com>
2017-07-12 22:16:31 -0700

    Update 2

--- app/scripts/requests.js
+++ app/scripts/requests.js
@@ -52,8 +52,8 @@ for (var i = 0; i < 8; i++) {
 	
 request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       text = this.responseText;
-	   localStorage.setItem("temp", text);
+       //text = this.responseText;
+	   localStorage.temp = this.responseText;
 		//document.getElementById('app').innerHTML += text;
 		
 	} // End of " If " statement
